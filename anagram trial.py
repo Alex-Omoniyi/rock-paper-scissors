a7f7866e8f50fe8dde49d0ce04917dce68e893a6
Alex-Omoniyi Isaac <isaac.alexomoniyi@gmail.com>
2022-09-06 10:19:26 +0100

    I have put it the code in a loop

---
+++ anagram trial.py
@@ -0,0 +1,13 @@
+
+user_action= input("Enter the level you want to play (from 1-5):")
+print(user_action)
+
+Level_1= "commentary " 
+Level_2="information"
+Level_3="education"
+Level_4="mathematics"
+Level_5="communication"
+if user_action== 'Level_1':
+    print("Your word is (metacomnry)")
+operation= input('Your answer(s):')
+
